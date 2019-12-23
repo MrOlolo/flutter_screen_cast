a4561dcb093064071e01319581c24c42f7e2e392
Konstantin Ignatiev <konstantin.ignatiev@spbpu.com>
2019-12-23 03:34:40 +0300

    add files

---
+++ android/app/src/main/kotlin/com/blackteam/flutter_screen_cast/MainActivity.kt
@@ -0,0 +1,386 @@
+package com.blackteam.flutter_screen_cast
+
+import android.annotation.TargetApi
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.graphics.PixelFormat
+import android.graphics.Point
+import android.hardware.display.DisplayManager
+import android.hardware.display.VirtualDisplay
+import android.media.Image
+import android.media.ImageReader
+import android.media.projection.MediaProjection
+import android.media.projection.MediaProjectionManager
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.Display
+import android.view.OrientationEventListener
+import android.view.Surface
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.blackteam.flutter_screen_cast.tcp_client.TcpClient
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.net.InetAddress
+import java.net.UnknownHostException
+
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
+class MainActivity : FlutterActivity() {
+    private val streamController = "ivt.black/stream_controller"
+    private val TAG = MainActivity::class.java.name
+    private val REQUEST_CODE = 100
+    private val FPS = 24
+    private val STORE_DIRECTORY: String? = null
+    private var IMAGES_PRODUCED: Int = 0
+    private val SCREENCAP_NAME = "screencap"
+    private val VIRTUAL_DISPLAY_FLAGS = DisplayManager.VIRTUAL_DISPLAY_FLAG_OWN_CONTENT_ONLY or DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC
+    private var sMediaProjection: MediaProjection? = null
+
+    private var mProjectionManager: MediaProjectionManager? = null
+    private var mImageReader: ImageReader? = null
+    private var mHandler: Handler? = null
+    private var mDisplay: Display? = null
+    private var mVirtualDisplay: VirtualDisplay? = null
+    private var mDensity: Int = 0
+    private var mWidth: Int = 0
+    private var mHeight: Int = 0
+    private var mRotation: Int = 0
+    private var mOrientationChangeCallback: OrientationChangeCallback? = null
+
+    private var tcpSocketClient: TcpClient? = null
+    private var remoteHost: InetAddress? = null
+    private val remotePort: Int = 0
+
+    private lateinit var _result: MethodChannel.Result
+    private var resultSended = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+
+        mProjectionManager = getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+
+        // start projection
+//        startButton.setOnClickListener {
+//            startProjection()
+//            if (!createSocket()) {
+//                Log.e(TAG, "Failed to connect tcp://$remoteHost:$remotePort")
+//            }
+//            Log.i(TAG, "TCP Socket created.")
+//        }
+
+        // stop projection
+//        stopButton.setOnClickListener {
+//            stopProjection()
+//            closeSocket()
+//        }
+
+        // start capture handling thread
+        object : Thread() {
+            override fun run() {
+                Looper.prepare()
+                mHandler = Handler()
+                Looper.loop()
+            }
+        }.start()
+
+        MethodChannel(flutterView, streamController).setMethodCallHandler { call, result ->
+            when (call.method) {
+
+                "start"
+                -> {
+                    resultSended = false
+                    _result = result
+                    // thread().start()
+                    // _result.success("started")
+//                    return
+//
+                    try {
+                        if (!createSocket()) {
+                            Log.e(TAG, "Failed to connect tcp://$remoteHost:$remotePort")
+                            if (!resultSended) {
+                                result.error("1", "Failed to connect tcp://$remoteHost:$remotePort", null)
+                                resultSended = true
+                            }
+                        } else {
+                            Log.i(TAG, "TCP Socket created.")
+                            startProjection()
+                        }
+                    } catch (t: Throwable) {
+                        if (!resultSended) {
+                            result.error("1", t.toString(), null)
+                            resultSended = true
+                        }
+                    }
+                }
+                "stop" -> {
+                    resultSended = false
+                    _result = result
+
+                    stopProjection()
+                    closeSocket()
+                    if (!resultSended) {
+                        result.success("stopped")
+                        resultSended = true
+                    }
+                }
+            }
+        }
+    }
+
+    private inner class OrientationChangeCallback internal constructor(context: Context) : OrientationEventListener(context) {
+
+        @TargetApi(Build.VERSION_CODES.KITKAT)
+        override fun onOrientationChanged(orientation: Int) {
+            Log.e(TAG, "change orientation")
+            try {
+                val rotation = mDisplay!!.rotation
+                if (rotation != mRotation) {
+                    mRotation = rotation
+                    if (mVirtualDisplay != null) {
+                        mVirtualDisplay?.release()
+                    }
+                    if (mImageReader != null) {
+                        mImageReader?.setOnImageAvailableListener(null, null)
+                    }
+
+                    createVirtualDisplay()
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+        }
+    }
+
+    private inner class ImageAvailableListener : ImageReader.OnImageAvailableListener {
+        private var lastImageMillis: Long = 0
+
+        private var reusableBitmap: Bitmap? = null
+
+        override fun onImageAvailable(reader: ImageReader) {
+            val os: ByteArrayOutputStream
+            var bitmap: Bitmap? = null
+
+            try {
+                val image = reader.acquireLatestImage()
+                if (image != null) {
+                    val now = System.currentTimeMillis()
+                    if (now - lastImageMillis < 1000 / FPS) {
+                        Log.e(TAG, "skip image")
+                        image.close()
+                        return
+                    }
+                    lastImageMillis = now
+
+                    //                    // create bitmap
+                    bitmap = getCleanBitmap(image)
+                    image.close()
+                    //Bitmap resizedBitmap = getResizedBitmap(bitmap, 1920, 1080);
+                    os = ByteArrayOutputStream()
+                    bitmap!!.compress(Bitmap.CompressFormat.JPEG, 30, os)
+                    sendData(null, os.toByteArray())
+                    //                    // write bitmap to a file
+                    //                    fos = new FileOutputStream(STORE_DIRECTORY + "/myscreen_" + IMAGES_PRODUCED + ".png");
+                    //                    bitmap.compress(CompressFormat.JPEG, 50, fos);
+
+                    os.close()
+                    IMAGES_PRODUCED++
+                    Log.e(TAG, "captured image: $IMAGES_PRODUCED")
+                }
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+
+                bitmap?.recycle()
+
+                //                if (image != null) {
+                //                    image.close();
+                //                }
+            }
+        }
+
+        private fun getCleanBitmap(image: Image): Bitmap? {
+            val plane = image.planes[0]
+            val width = plane.rowStride / plane.pixelStride
+            val cleanBitmap: Bitmap
+            if (width > image.width) {
+                reusableBitmap = Bitmap.createBitmap(width, image.height, Bitmap.Config.ARGB_8888)
+
+                reusableBitmap!!.copyPixelsFromBuffer(plane.buffer)
+                cleanBitmap = Bitmap.createBitmap(reusableBitmap!!, 0, 0, image.width, image.height)
+            } else {
+                cleanBitmap = Bitmap.createBitmap(image.width, image.height, Bitmap.Config.ARGB_8888)
+                cleanBitmap.copyPixelsFromBuffer(plane.buffer)
+            }
+            return cleanBitmap
+        }
+
+        fun getResizedBitmap(bm: Bitmap, newWidth: Int, newHeight: Int): Bitmap {
+            val width = bm.width
+            val height = bm.height
+            val scaleWidth = newWidth.toFloat() / width
+            val scaleHeight = newHeight.toFloat() / height
+            // CREATE A MATRIX FOR THE MANIPULATION
+            val matrix = Matrix()
+            // RESIZE THE BIT MAP
+            if (scaleWidth > scaleHeight) {
+                matrix.postScale(scaleWidth, scaleWidth)
+            } else {
+                matrix.postScale(scaleHeight, scaleHeight)
+            }
+
+            // "RECREATE" THE NEW BITMAP
+            val resizedBitmap = Bitmap.createBitmap(
+                    bm, 0, 0, width, height, matrix, false)
+            bm.recycle()
+            return resizedBitmap
+        }
+
+    }
+
+    private inner class MediaProjectionStopCallback : MediaProjection.Callback() {
+        override fun onStop() {
+            Log.e("ScreenCapture", "stopping projection.")
+            mHandler!!.post {
+                if (mVirtualDisplay != null) mVirtualDisplay?.release()
+                if (mImageReader != null) mImageReader?.setOnImageAvailableListener(null, null)
+                if (mOrientationChangeCallback != null) mOrientationChangeCallback?.disable()
+                sMediaProjection?.unregisterCallback(this@MediaProjectionStopCallback)
+            }
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode != REQUEST_CODE) {
+            Log.e(TAG, "Unknown request code: $requestCode")
+            if (!resultSended) {
+                _result.error("0", "STRANGE CASE", null)
+                resultSended = true
+            }
+            return
+        }
+        if (resultCode != RESULT_OK) {
+            Toast.makeText(this,
+                    "Screen Cast Permission Denied", Toast.LENGTH_SHORT).show()
+            if (!resultSended) {
+                _result.error("1", "access denied", null)
+                resultSended = true
+            }
+            return
+        }
+        if (requestCode == REQUEST_CODE) {
+            sMediaProjection = mProjectionManager?.getMediaProjection(resultCode, data!!)
+
+            if (sMediaProjection != null) {
+                // display metrics
+                val metrics = resources.displayMetrics
+                mDensity = metrics.densityDpi
+                mDisplay = windowManager.defaultDisplay
+
+                // create virtual display depending on device width / height
+                createVirtualDisplay()
+
+                // register orientation change callback
+                mOrientationChangeCallback = OrientationChangeCallback(this)
+                if (mOrientationChangeCallback!!.canDetectOrientation()) {
+                    mOrientationChangeCallback?.enable()
+                }
+
+                // register media projection stop callback
+                sMediaProjection?.registerCallback(MediaProjectionStopCallback(), mHandler)
+                if (!resultSended) {
+                    _result.success("started")
+                    resultSended = true
+                }
+            }
+        }
+    }
+
+    /****************************************** UI Widget Callbacks  */
+    private fun startProjection() {
+        startActivityForResult(mProjectionManager?.createScreenCaptureIntent(), REQUEST_CODE)
+    }
+
+    private fun stopProjection() {
+        mHandler!!.post {
+            if (sMediaProjection != null) {
+                sMediaProjection?.stop()
+            }
+        }
+    }
+
+    /****************************************** Factoring Virtual Display creation  */
+    private fun createVirtualDisplay() {
+        // get width and height
+        val size = Point()
+        val rotation = mDisplay?.rotation
+        mDisplay?.getSize(size)
+        if (rotation == Surface.ROTATION_0 || rotation == Surface.ROTATION_180) {
+            mWidth = if (size.x > size.y) size.y else size.x
+            mHeight = if (size.x > size.y) size.x else size.y
+        } else {
+            mWidth = if (size.x > size.y) size.x else size.y
+            mHeight = if (size.x > size.y) size.y else size.x
+        }
+
+        // start capture reader
+        mImageReader = ImageReader.newInstance(mWidth, mHeight, PixelFormat.RGBA_8888, 5)
+        mVirtualDisplay = sMediaProjection?.createVirtualDisplay(SCREENCAP_NAME, mWidth, mHeight, mDensity, VIRTUAL_DISPLAY_FLAGS, mImageReader!!.surface, null, mHandler)
+        mImageReader?.setOnImageAvailableListener(ImageAvailableListener(), mHandler)
+    }
+
+    private fun sendData(header: ByteArray?, data: ByteArray) {
+        if (tcpSocketClient != null) {
+            if (header != null) {
+                tcpSocketClient?.send(header)
+            }
+            Log.e(TAG, "data " + data.size)
+            tcpSocketClient?.send(data)
+        } else {
+            Log.e(TAG, "tcp socket not available.")
+            stopProjection()
+        }
+    }
+
+    private fun createSocket(): Boolean {
+        val kk = "192.168.1.186"
+        try {
+            this.remoteHost = InetAddress.getByName(kk)
+        } catch (e: UnknownHostException) {
+            Toast.makeText(this,
+                    e.message, Toast.LENGTH_SHORT).show()
+            e.printStackTrace()
+        }
+
+        tcpSocketClient = TcpClient(remoteHost!!, 49152)
+        tcpSocketClient?.start()
+        return true
+    }
+
+    private fun closeSocket() {
+        if (tcpSocketClient != null) {
+            try {
+                tcpSocketClient?.close()
+            } catch (ex: Exception) {
+                ex.printStackTrace()
+            } finally {
+                tcpSocketClient = null
+            }
+        }
+    }
+
+
+}
---
+++ android/app/src/main/kotlin/com/blackteam/flutter_screen_cast/tcp_client/TcpClient.kt
@@ -0,0 +1,87 @@
+package com.blackteam.flutter_screen_cast.tcp_client
+
+import android.annotation.TargetApi
+import android.os.Build
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.util.Log
+import java.io.BufferedOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.net.InetAddress
+import java.net.Socket
+
+class TcpClient(val remoteHost: InetAddress, val remotePort: Int) : Thread() {
+    private val TAG = "TcpSocketClient"
+
+    private var socket: Socket? = null
+    private var outputStream: OutputStream? = null
+    private var bufferedOutputStream: BufferedOutputStream? = null
+    private var handler: Handler? = null
+
+    @TargetApi(Build.VERSION_CODES.CUPCAKE)
+    internal inner class IncomingHandlerCallback : Handler.Callback {
+
+        override fun handleMessage(message: Message?): Boolean {
+
+            if (message?.obj == null) return false
+            val msg = message.obj as ByteArray
+            try {
+                bufferedOutputStream?.write(msg)
+            } catch (e: IOException) {
+                e.printStackTrace()
+                close()
+            }
+            return true
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.CUPCAKE)
+    override fun run() {
+        try {
+            socket = Socket(remoteHost, remotePort)
+            outputStream = socket?.getOutputStream()
+            bufferedOutputStream = BufferedOutputStream(outputStream)
+        } catch (e: IOException) {
+            Log.e(TAG, "Socket creation failed - $e")
+            socket = null
+            outputStream = null
+            bufferedOutputStream = null
+        }
+
+
+        Looper.prepare()
+        handler = Handler(IncomingHandlerCallback())
+        Looper.loop()
+
+    }
+
+    fun close() {
+        if (socket != null) {
+            try {
+                socket!!.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            } finally {
+                socket = null
+                outputStream = null
+                bufferedOutputStream = null
+            }
+        }
+    }
+
+    fun send(data: ByteArray) {
+        Log.e(TAG, "send data")
+        if (handler == null || socket == null || outputStream == null) {
+            return
+        }
+        val message = handler!!.obtainMessage()
+        message.obj = data
+
+        Log.e(TAG, "send data 2")
+        Log.e(TAG, "size: " + data.size)
+        handler!!.sendMessage(message)
+    }
+
+}
